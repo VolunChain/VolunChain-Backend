a3024849dce9b90e294998a9eb164d360f9611c9
bosunUbuntu <joshuaibitoye111@gmail.com>
2025-04-28 23:57:54 +0100

    added back comment in AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,3 +1,24 @@
+// import jwt from "jsonwebtoken";
+// import { prisma } from "../config/prisma";
+
+// const SECRET_KEY = process.env.JWT_SECRET || "defaultSecret";
+
+// class AuthService {
+//   async authenticate(walletAddress: string): Promise<string> {
+//     const user = await prisma.user.findUnique({
+//       where: { wallet: walletAddress },
+//     });
+
+//     if (!user) {
+//       throw new Error("User not found");
+//     }
+
+//     return jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: "1h" });
+//   }
+// }
+
+// export default AuthService;
+
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { prisma } from "../config/prisma";
